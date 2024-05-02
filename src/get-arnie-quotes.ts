b431c541f05fffb08a04a195e3709e95778489d9
Arthur Wong <a.wong45789@gmail.com>
2024-05-02 20:56:51 +1000

    submission by Arthur Wong

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,23 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+type Keys = "FAILURE" | "Arnie Quote";
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+type TResult = { [K in Keys]?: string };
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const getPromises = urls.map(async (url): Promise<TResult> => {
+    try {
+      const res = await httpGet(url);
+      const message = JSON.parse(res.body)?.["message"] ?? "";
+      if (res.status === 200) {
+        return { "Arnie Quote": message };
+      } else {
+        return { FAILURE: message };
+      }
+    } catch (err) {
+      return { FAILURE: JSON.stringify(err) };
+    }
+  });
+
+  return Promise.all(getPromises);
 };
